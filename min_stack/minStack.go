3a76f8677709cc1df9e99f7bacc5bfb447c44d05
zhangxu <1304690116@qq.com>
2021-04-21 14:43:57 +0800

    solve the min stack 2021/4/21

--- min_stack/minStack.go
+++ min_stack/minStack.go
@@ -1,1 +1,35 @@
 package min_stack
+
+type MinStack struct {
+	stack []item
+}
+
+type item struct {
+	val, min int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		stack: []item{},
+	}
+}
+
+func (this *MinStack) push(val int) {
+	min := val
+	if len(this.stack) > 0 && this.GetMin() < min {
+		min = this.GetMin()
+	}
+	this.stack = append(this.stack, item{val: val, min: min})
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1].val
+}
+
+func (this *MinStack) GetMin() int {
+	return this.stack[len(this.stack)-1].min
+}

5f548adbbb5efaa77fd073947bacfd05200bd64b
zhangxu1814 <1304690116@qq.com>
2022-08-15 20:57:16 +0800

    add solution 327 493

---
+++ leetcode/binaryTree/0493_reverse_pairs.go
@@ -0,0 +1,49 @@
+package binaryTree
+
+func reversePairs(nums []int) int {
+	tmp := make([]int, len(nums))
+	var l, r, end, count int
+	var sort func(int, int)
+	var merge func(int, int, int)
+	sort = func(lo, hi int) {
+		if lo == hi {
+			return
+		}
+		mid := lo + (hi-lo)/2
+		sort(lo, mid)
+		sort(mid+1, hi)
+		merge(lo, mid, hi)
+	}
+	merge = func(lo, mid, hi int) {
+		end = mid + 1
+		for i := lo; i <= mid; i++ {
+			for end <= hi && nums[i] > nums[end]*2 {
+				end++
+			}
+			count += end - mid - 1
+		}
+
+		for i := lo; i <= hi; i++ {
+			tmp[i] = nums[i]
+		}
+		l, r = lo, mid+1
+		for i := lo; i <= hi; i++ {
+			if l > mid {
+				nums[i] = tmp[r]
+				r++
+			} else if r > hi {
+				nums[i] = tmp[l]
+				l++
+			} else if tmp[l] > tmp[r] {
+				nums[i] = tmp[r]
+				r++
+			} else {
+				nums[i] = tmp[l]
+				l++
+			}
+		}
+	}
+
+	sort(0, len(nums)-1)
+	return count
+}
